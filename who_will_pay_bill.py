5727b5659da2135ba291378c207f22df68881eb5
Pranav Makote <80123831+pranavmak@users.noreply.github.com>
2025-10-14 14:04:10 +0530

    who will pay bill

---
+++ who_will_pay_bill.py
@@ -0,0 +1,26 @@
+import random
+
+friend = ["pranav", "akshay", "saif", "kiran"]
+
+#option 1
+
+# random_num = random.randint(0,3)
+# if random_num == 0:
+#     print(friend[0])
+# elif random_num == 1:
+#     print(friend[1])
+# elif random_num == 2:
+#     print(friend[2])
+# else:
+#     print(friend[3])
+
+#option 2
+
+# print(random.choice(friend))
+
+
+#option 3
+
+random_index = random.randint(0,3)
+print(friend[random_index])
+
