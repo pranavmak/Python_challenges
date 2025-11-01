9f5d76c9030a54d1728a80909aef906abefd6d04
Pranav Makote <80123831+pranavmak@users.noreply.github.com>
2025-11-01 23:20:47 +0530

    Enhance password generator with uppercase letters
    Added uppercase letters and print statement for password.

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,6 @@
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')']
 
@@ -40,5 +40,6 @@ random.shuffle(password_list)
 
 # Step 3: join the shuffled list into a string
 password = ''.join(password_list)
+
 
 print(f"Your random password is: {password}")
